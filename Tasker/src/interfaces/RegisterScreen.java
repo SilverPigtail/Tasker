8a92153f146d49b999f54a0e36c54525126f0771
Bjornx <carlos.y.rocky13@gmail.com>
2019-05-14 13:22:33 +0200

    Versión 0.6 -> Added one exceptions and edited graphic interfaces.

--- Tasker/src/interfaces/RegisterScreen.java
+++ Tasker/src/interfaces/RegisterScreen.java
@@ -9,51 +9,59 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
-public class RegisterUser extends JPanel{
+public class RegisterScreen extends JPanel{
 	private JTextField nickTextField;
 	private JTextField emailField;
 	private JPasswordField passwordRegField;
-	public RegisterUser() {
+	
+	public RegisterScreen(Window w) {
 		setBackground(new Color(102, 153, 255));
 		setLayout(null);
 		
 		JLabel lblTaskerSignup = new JLabel("Tasker Sign-up");
+		lblTaskerSignup.setFont(new Font("Verdana", Font.BOLD, 16));
 		lblTaskerSignup.setBackground(new Color(0, 0, 0));
-		lblTaskerSignup.setBounds(193, 11, 77, 14);
+		lblTaskerSignup.setBounds(193, 11, 159, 34);
 		add(lblTaskerSignup);
 		
 		JLabel nicknameLabel = new JLabel("Nickname:");
 		nicknameLabel.setFont(new Font("Arial", Font.BOLD, 17));
-		nicknameLabel.setBounds(56, 120, 94, 29);
+		nicknameLabel.setBounds(41, 126, 94, 29);
 		add(nicknameLabel);
 		
 		JLabel emailLabel = new JLabel("E-mail:");
 		emailLabel.setFont(new Font("Arial", Font.BOLD, 17));
-		emailLabel.setBounds(56, 193, 94, 29);
+		emailLabel.setBounds(41, 207, 94, 29);
 		add(emailLabel);
 		
 		JLabel passwordLabel = new JLabel("Password:");
 		passwordLabel.setFont(new Font("Arial", Font.BOLD, 17));
-		passwordLabel.setBounds(56, 263, 106, 40);
+		passwordLabel.setBounds(41, 277, 106, 40);
 		add(passwordLabel);
 		
 		nickTextField = new JTextField();
-		nickTextField.setBounds(184, 126, 136, 23);
+		nickTextField.setBounds(179, 130, 189, 40);
 		add(nickTextField);
 		nickTextField.setColumns(10);
 		
 		emailField = new JTextField();
-		emailField.setBounds(184, 199, 141, 23);
+		emailField.setBounds(179, 209, 189, 29);
 		add(emailField);
 		emailField.setColumns(10);
 		
 		passwordRegField = new JPasswordField();
-		passwordRegField.setBounds(179, 275, 149, 23);
+		passwordRegField.setBounds(179, 275, 194, 29);
 		add(passwordRegField);
 		
 		JButton registerButton = new JButton("SING-UP!");
-		registerButton.setBounds(181, 359, 94, 40);
+		registerButton.setBounds(194, 361, 113, 40);
 		add(registerButton);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\1DAM\\Documents\\Tasker\\Tasker\\resources\\register_background.png"));
+		lblNewLabel.setBounds(0, 0, 500, 500);
+		add(lblNewLabel);
 	}
 }
--- Tasker/src/interfaces/Window.java
+++ Tasker/src/interfaces/Window.java
@@ -5,23 +5,74 @@ import javax.swing.JFrame;
 import user.User;
 
 public class Window extends JFrame{
-	private RegisterUser RegisterUser;
-	private StartScreen StartScreen;
+	private RegisterScreen registerS;
+	private LoginScreen loginS;
+	private StartScreen startS;
 	private User user;
 	
 	
 	public Window() {
 		super();
+		setResizable(false);
 		this.setTitle("Registro");
-		StartScreen=new StartScreen(this);
-		this.setSize(600,600);
+		startS=new StartScreen(this);
+		this.setSize(500,500);
 		this.setVisible(true);
-		this.setContentPane(StartScreen);
+		this.setContentPane(startS);
 		
 		
 		
 		
+	
 		
+		
+	}
+	
+	
+	public void loadRegisterScreen() {
+		
+		if(this.registerS==null) {
+			
+			this.registerS=new RegisterScreen(this);
+			
+		}else {
+			this.setTitle("Sing-Up");
+			this.startS.setVisible(false);
+			this.setContentPane(this.registerS);
+		}
+		
+		
+	}
+	
+	
+	public void loadLoginScreen() {
+		
+		if(this.loginS==null) {
+			
+			this.loginS=new LoginScreen(this);
+			
+		}else {
+			this.setTitle("Sign-In");
+			this.startS.setVisible(false);
+			this.setContentPane(this.loginS);
+		}
+		
+		
+	}
+	
+	public void loadStartScreen() {
+		
+		this.setTitle("Tasker, all what do you have to do here!");
+		if(this.loginS!=null) {
+			
+			this.loginS.setVisible(false);
+			
+		}else if(this.registerS!=null) {
+			
+			this.registerS.setVisible(false);
+		}
+		this.startS.setVisible(true);
+		this.setContentPane(startS);
 	}
 	
 }
